fc10eb4a59ba704f3cca51ee981fdfe8e5b5393d
Aadarsh Singh <aadarshsingh121@gmail.com>
2024-04-08 10:04:43 +0530

    Update videoLoader.js
    change the video Loder Lib function behavior.

--- videoLoader/videoLoader.js
+++ videoLoader/videoLoader.js
@@ -1,12 +1,12 @@
 import { loadVideo } from "../loader.js";
 import * as THREE from "three";
 
-export function videoLoader({ path }) {
+export function videoLoader({ path, ratio=0.5 }) {
   return new Promise(async (resolve, reject) => {
     const video = await loadVideo(path);
     const texture = new THREE.VideoTexture(video);
 
-    const geometry = new THREE.PlaneGeometry(1, 0.5);
+    const geometry = new THREE.PlaneGeometry(1, ratio);
     const material = new THREE.MeshBasicMaterial({ map: texture });
 
     const videoPlane = new THREE.Mesh(geometry, material);
